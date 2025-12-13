931dcaa30548936706417666d8582c9bab71fb53
KingWahley <kingwahley@gmail.com>
2025-12-13 18:33:17 +0100

    header changes

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,8 @@
 export default function Header() {
   return (
     <header className="space-y-2">
-      <h1 className="text-4xl font-bold">Fake Client Lab</h1>
-      <p className="text-zinc-400">Practice real-world Client briefs.</p>
+      <h1 className="text-4xl font-bold">Fake Client </h1>
+      <p className="text-zinc-400">Practice real-world Client briefs</p>
     </header>
   );
 }
