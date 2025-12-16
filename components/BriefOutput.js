480dc5a162050ad5690798ff5aa037ae0f266326
KingWahley <kingwahley@gmail.com>
2025-12-16 14:59:42 +0100

    made changes to prompt

--- components/BriefOutput.js
+++ components/BriefOutput.js
@@ -1,11 +1,12 @@
 export default function BriefOutput({ brief }) {
+
+
   return (
     <div className="bg-zinc-900 p-6 rounded whitespace-pre-wrap leading-relaxed">
       {brief}
     </div>
   );
 }
-
 
 // typewrter feauture below
 
